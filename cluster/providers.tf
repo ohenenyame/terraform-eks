231b4809910d9cf3bc12421c83e316b1a70df560
Anthony Ohene <ohenea@pc2051.ebs.msft>
2022-03-11 13:13:18 +0000

    new update provider

--- cluster/providers.tf
+++ cluster/providers.tf
@@ -2,9 +2,17 @@
 # Provider Configuration
 #
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "us-west-2"
-  version = "~> 1.24"
 }
 
 # Using these data sources allows the configuration to be
@@ -13,10 +21,3 @@ data "aws_region" "current" {}
 
 data "aws_availability_zones" "available" {}
 
-# Not required: currently used in conjuction with using
-# icanhazip.com to determine local workstation external IP
-# to open EC2 Security Group access to the Kubernetes cluster.
-# See workstation-external-ip.tf for additional information.
-provider "http" {
-  version = "~> 1.0"
-}
